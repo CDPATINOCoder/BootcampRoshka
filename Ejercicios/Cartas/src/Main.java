4b6d3f9aad569d8097af0280b22840868e2f2f15
Patiño Vera César David <cesardavidpatinovera@gmail.com>
2024-11-28 00:23:55 -0300

    agregar Cartas

---
+++ Ejercicios/Cartas/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Carta> cartas = new ArrayList<>();
+        cartas.add(new Carta("2D"));
+        cartas.add(new Carta("6H"));
+        cartas.add(new Carta("7S"));
+        cartas.add(new Carta("3D"));
+        cartas.add(new Carta("5S"));
+
+
+
+        String jugadaDevueltaJugador1 = JuegoPoker.determinarJugada(cartas);
+        System.out.println("La jugada es: " + jugadaDevueltaJugador1);
+
+    }
+
+
+
+
+}
