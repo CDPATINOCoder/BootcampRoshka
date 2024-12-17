640c01a65017f4f2253feee91e0ecefc1cc24f2b
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2024-12-17 15:54:47 -0300

    Manejo de errores y postPostulante agregados para el put

---
+++ Web/api/src/main/java/com/postulantes/api/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.postulantes.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.postulantes.api.responses.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
